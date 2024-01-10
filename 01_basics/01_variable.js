c88f2a13a1cd2594950983b7ab1752c456190710
Tanya Srivastava <tanyasrivastava2808@gmail.com>
2024-01-10 18:23:07 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 14333
+let accountEmail = "abc@gmail.com"
+let accountPassword = 1234
+
+accountEmail = "adc@gamil.com"
+accountPassword = 235
+
+
+console.table([ accountId, accountEmail, accountPassword])
+console.log(accountId);
---
+++ 01_basics/02_conversion.js
@@ -0,0 +1,7 @@
+let score = "true"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
