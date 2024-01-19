365cf686ef0e700803b0117ccf63571916a234ca
Tanya Srivastava <tanyasrivastava2808@gmail.com>
2024-01-19 14:40:04 +0000

    Container config done

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -21,5 +21,26 @@ console.log(regularUser.fullname.userfullname);
 const obj1 = {1:"a",2:"b"}
 const obj2 = {3:"c",d:"4"}
 
-const obj3= Object.assign(obj1,obj2)
+//const obj3= Object.assign(obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
 console.log(obj3);
+
+const users = [
+    {
+      id:1,
+      email:"t@gmail.com"
+    },
+    {
+        id:1,
+        email:"t@gmail.com"
+      },
+      {
+        id:1,
+        email:"t@gmail.com"
+      },
+]
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
