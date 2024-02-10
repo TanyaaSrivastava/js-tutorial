108b75bed95533225b7867f2712e90f51580ac4f
Tanya Srivastava <tanyasrivastava2808@gmail.com>
2024-02-10 19:31:56 +0000

    control_flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,4 +1,10 @@
 const isUserLoggedIn = true
+const temperature = 41
+
+if(temperature < 50){
+    console.log("less than 50");
+}
+console.log("temperature is greater than 50");
 
 if (2 == "2"){
     console.log("executed");
