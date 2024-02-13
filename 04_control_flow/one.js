786b9017b962216154903b027f25d38d460895f5
Tanya Srivastava <tanyasrivastava2808@gmail.com>
2024-02-13 14:07:45 +0000

    control_flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -3,10 +3,19 @@ const temperature = 41
 
 if(temperature < 50){
     console.log("less than 50");
-}
+}else
 console.log("temperature is greater than 50");
 
 if (2 == "2"){
     console.log("executed");
+
+const score = 200
+
+if(score  > 100) {
+    const power = "fly"
+    console.log(`User power: ${power}`);
+}
+
+console.log(`User power: ${power}`);
 
 }
