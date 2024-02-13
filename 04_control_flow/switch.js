8f2df1b7ce79238c2ee85d3830d491ce31c6cbd0
Tanya Srivastava <tanyasrivastava2808@gmail.com>
2024-02-13 19:29:32 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,15 @@
+const month = 3
+
+
+switch (month) {
+    
+    case 1:
+        console.log("January");
+        break;
+        case 2:
+            console.log("feb");
+            break;
+          
+    default:
+        console.log("default value");
+}
