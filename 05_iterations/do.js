9f74e751610813ec52429cb1c5e75796f54fdb5b
Tanya Srivastava <tanyasrivastava2808@gmail.com>
2024-02-16 02:25:45 +0000

    iterations

--- 05_iterations/do.js
+++ 05_iterations/do.js
@@ -3,3 +3,9 @@ while (index <= 10) {
     console.log(`value of index ${index}`);
     index = index + 2
 }
+let myArray = ["batman","spiderman","flash"]
+let arr = 0
+while(arr< myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
