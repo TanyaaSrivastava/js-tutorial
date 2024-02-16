160357bdafb4fa57686202ae774945d5fb778d44
Tanya Srivastava <tanyasrivastava2808@gmail.com>
2024-02-16 02:25:24 +0000

    map

---
+++ 05_iterations/three.js
@@ -0,0 +1,9 @@
+const greetings = "hello world"
+for(const greet of greetings){
+    console.log(`each char is ${greet}`)
+}
+const map = new Map()
+map.set ("IN","India")
+map.set("USA","United State of America")
+map.set("FR","France")
+console.log(map);
